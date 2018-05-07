cc7c2ab7a2557be34e1008ca97e0e7494964d166
TrafalgarRicardoLu <trafalgarricardolu@gmail.com>
2018-05-07 22:16:46 +0800

    feat: create basic UI

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,24 +1,59 @@
 package view;
 
+import com.sun.xml.internal.bind.v2.TODO;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TitledPane;
+import javafx.scene.control.cell.ComboBoxListCell;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
  * @author trafalgar
  */
 public class Main extends Application {
+
+
+
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Main.fxml"));
-        Scene scene = new Scene(root, 600, 500);
+        ListView eventList = new ListView();
 
-        primaryStage.setTitle("Simple JavaFX");
+        ListView labelList = new ListView();
+        ListView projectList = new ListView();
+        ListView priorityList = new ListView();
+
+        /*
+        /TODO
+        /read map info and put it to List
+        */
+
+        TitledPane label = new TitledPane("Label",labelList);
+        TitledPane project = new TitledPane("Project",projectList);
+        TitledPane priority = new TitledPane("Priority",priorityList);
+
+        Accordion leftView = new Accordion();
+        leftView.getPanes().addAll(label,project,priority);
+
+        HBox root = new HBox();
+        root.getChildren().addAll(leftView, eventList);
+
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
