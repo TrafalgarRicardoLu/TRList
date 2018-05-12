9c4bdf612d58600613ca66a17f5b71467c9cbcc8
TrafalgarRicardoLu <trafalgarricardolu@gmail.com>
2018-05-12 18:58:48 +0800

    refactor: use singleton pattern to build Maps

--- src/main/java/entity/Maps.java
+++ src/main/java/entity/Maps.java
@@ -1,23 +1,80 @@
 package entity;
 
 import org.apache.commons.collections4.list.TreeList;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import utils.XMLGenerator;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * @author trafalgar
  */
 public class Maps {
+
+    private static volatile Maps maps= null;
+
     private static HashMap<String, List<Event>> labelMap = new HashMap<>();
 
     private static HashMap<String, List<Event>> priorityMap = new HashMap<>();
 
     private static HashMap<String, List<Event>> projectMap = new HashMap<>();
 
-    private static void initMaps() {
+
+    private Maps() throws DocumentException {
+        List<String> labelFilter = XMLGenerator.getFilterNamesByMenuName("label");
+        List<String> projectFilter = XMLGenerator.getFilterNamesByMenuName("project");
+        List<String> priorityFilter = XMLGenerator.getFilterNamesByMenuName("priority");
+
+        for(String filterName: labelFilter){
+            List<Event> events = new LinkedList<>();
+            List<String> labelUids = XMLGenerator.getUidsByFilterName("label",filterName);
+
+            for(String uid:labelUids){
+                //Todo
+                //get event by UID
+                //events.add();
+            }
+            labelMap.put(filterName,events);
+        }
+        for(String filterName: projectFilter){
+            List<Event> events = new LinkedList<>();
+            List<String> projectUids = XMLGenerator.getUidsByFilterName("project",filterName);
+
+            for(String uid:projectUids){
+                //Todo
+                //get event by UID
+                //events.add();
+            }
+            labelMap.put(filterName,events);
+        }
+        for(String filterName: priorityFilter){
+            List<Event> events = new LinkedList<>();
+            List<String> priorityUids = XMLGenerator.getUidsByFilterName("priority",filterName);
+
+            for(String uid:priorityUids){
+                //Todo
+                //get event by UID
+                //events.add();
+            }
+            labelMap.put(filterName,events);
+        }
 
     }
+
+    public static Maps getMaps() throws DocumentException {
+        if(maps == null){
+            synchronized (Maps.class){
+                if(maps == null){
+                    maps = new Maps();
+                }
+            }
+        }
+        return maps;
+    }
+
 
     public static HashMap<String, List<Event>> getLabelMap() {
         return labelMap;
