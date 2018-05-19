fff37459970d9e452280e6141f6dcad2458cc00e
TrafalgarRicardoLu <trafalgarricardolu@gmail.com>
2018-05-19 13:52:28 +0800

    feat: enable app response to filter click

--- src/main/java/view/TRList.java
+++ src/main/java/view/TRList.java
@@ -1,6 +1,8 @@
 package view;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
@@ -9,13 +11,12 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-import utils.xml.XMLGenerator;
+import utils.MapHelper;
 
 /**
  * @author trafalgar
  */
 public class TRList extends Application {
-
 
 
     public static void main(String[] args) {
@@ -30,24 +31,55 @@ public class TRList extends Application {
         ListView projectList = new ListView();
         ListView priorityList = new ListView();
 
-        Object[] labelFilters = XMLGenerator.getFilterNamesByMenuName("label").toArray();
-        Object[] projectFilters = XMLGenerator.getFilterNamesByMenuName("project").toArray();
-        Object[] priorityFilters = XMLGenerator.getFilterNamesByMenuName("priority").toArray();
+        Object[] labelFilters = MapHelper.getFilterNamesByMenuName("label").toArray();
+        Object[] projectFilters = MapHelper.getFilterNamesByMenuName("project").toArray();
+        Object[] priorityFilters = MapHelper.getFilterNamesByMenuName("priority").toArray();
 
-        ObservableList<Object> labelItems =FXCollections.observableArrayList (labelFilters);
-        ObservableList<Object> projectItems =FXCollections.observableArrayList (projectFilters);
-        ObservableList<Object> priorityItems =FXCollections.observableArrayList (priorityFilters);
+        ObservableList<Object> labelItems = FXCollections.observableArrayList(labelFilters);
+        ObservableList<Object> projectItems = FXCollections.observableArrayList(projectFilters);
+        ObservableList<Object> priorityItems = FXCollections.observableArrayList(priorityFilters);
+
+
+        labelList.getSelectionModel().selectedItemProperty()
+                .addListener(new ChangeListener<String>() {
+                    @Override
+                    public void changed(
+                            ObservableValue<? extends String> observable,
+                            String oldValue, String newValue) {
+                        eventList.setItems((ObservableList) MapHelper.getEventByFilterName("label", newValue));
+                    }
+                });
+
+        projectList.getSelectionModel().selectedItemProperty()
+                .addListener(new ChangeListener<String>() {
+                    @Override
+                    public void changed(
+                            ObservableValue<? extends String> observable,
+                            String oldValue, String newValue) {
+                        eventList.setItems((ObservableList) MapHelper.getEventByFilterName("project", newValue));
+                    }
+                });
+
+        priorityList.getSelectionModel().selectedItemProperty()
+                .addListener(new ChangeListener<String>() {
+                    @Override
+                    public void changed(
+                            ObservableValue<? extends String> observable,
+                            String oldValue, String newValue) {
+                        eventList.setItems((ObservableList) MapHelper.getEventByFilterName("priority", newValue));
+                    }
+                });
 
         labelList.setItems(labelItems);
         projectList.setItems(projectItems);
         priorityList.setItems(priorityItems);
 
-        TitledPane label = new TitledPane("Label",labelList);
-        TitledPane project = new TitledPane("Project",projectList);
-        TitledPane priority = new TitledPane("Priority",priorityList);
+        TitledPane label = new TitledPane("Label", labelList);
+        TitledPane project = new TitledPane("Project", projectList);
+        TitledPane priority = new TitledPane("Priority", priorityList);
 
         Accordion leftView = new Accordion();
-        leftView.getPanes().addAll(label,project,priority);
+        leftView.getPanes().addAll(label, project, priority);
 
         HBox root = new HBox();
         root.getChildren().addAll(leftView, eventList);
